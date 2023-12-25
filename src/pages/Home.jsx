0086d62746e26c2c0ff89c67280bf092e3213bc8
myriam chahlafi <myriam.chah@gmail.com>
2023-12-25 22:18:51 +0800

    Add infinite scroll

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,15 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 import GameCard from "../components/Card/GameCard";
 
 const Home = () => {
   const [games, setGames] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [hasMore, setHasMore] = useState(true);
+  const [page, setPage] = useState(2);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,12 +27,28 @@ const Home = () => {
     fetchData();
   }, []);
 
+  const fetchMoreData = () => {
+    axios
+      .get(`http://localhost:3000?page=${page}`)
+      .then((res) => {
+        setGames((prevGames) => [...prevGames, ...res.data.results]);
+        res.data.results.length > 0 ? setHasMore(true) : setHasMore(false);
+      })
+      .catch((err) => console.log(err));
+    setPage((prevPage) => prevPage + 1);
+  };
+
   return (
-    <>
+    <Container>
       {isLoading ? (
         <div>Games list loading... </div>
       ) : (
-        <Container>
+        <InfiniteScroll
+          dataLength={games.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<div>... </div>}
+        >
           <Row>
             <h1>New and trending</h1>
             <p>Based on player counts and release date</p>
@@ -39,9 +58,9 @@ const Home = () => {
               <GameCard key={i} {...game} />
             ))}
           </div>
-        </Container>
+        </InfiniteScroll>
       )}
-    </>
+    </Container>
   );
 };
 
